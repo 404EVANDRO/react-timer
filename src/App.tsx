c75044353a5ddde2715ab23a081c00a1e969549d
404evandro <evandrocieslinsky@gmail.com>
2024-12-09 18:05:49 -0300

    Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,19 @@
-import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
+export function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          
-        </a>
-        <a href="https://react.dev" target="_blank">
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle/>
+    </ThemeProvider>
   )
 }
 
-export default App
